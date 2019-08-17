719bb6189283eae5332c889d252953217daeafc2
Dave Regg <drregg6@gmail.com>
2019-08-17 17:43:41 -0400

    Progress?

--- scripts.js
+++ scripts.js
@@ -12,11 +12,48 @@ Game Obj - Control the flow of the game
 */
 
 const BUTTON = document.querySelector('#play');
-BUTTON.addEventListener('click', (event) => {
-  console.log('Play game!');
-});
+const GAMESPACES = document.querySelectorAll('.gamespace');
+// console.log(GAMESPACES);
+// BUTTON.addEventListener('click', (event) => {
+//   if (!gamemode) {
+//     gamemode = true;
+//     console.log(gamemode);
+//   }
+// });
 
-const Gameplay = (gameboard, player1, player2) => {
+
+// for (let i = 0; i < GAMESPACES.length; i++) {
+//   GAMESPACES[i].addEventListener('click', function(event) {
+//     if (gamemode) {
+//       console.log(this);
+//     } else {
+//       console.log('Goodbye world...');
+//     }
+//   })
+// };
+
+function Gameplay() {
+  let gamemode = false;
+  const playerX = new Player('Player 1');
+  const playerO = new Player('Player 2', 'O');
+  const gameboard = new Gameboard();
+
+  const getGamemode = () => gamemode;
+  const startGame = () => {
+    if (!gamemode) {
+      gamemode = true;
+    } else {
+      console.log('Error');
+    }
+  }
+
+  const endGame = () => {
+    if (gamemode) {
+      gamemode = false;
+    } else {
+      console.log('Error');
+    }
+  }
   const checkWinner = (gameboard) => {
     // gameboard = [{},{},{},{},{},{},{},{},{}]
     // let count = 1;
@@ -29,13 +66,17 @@ const Gameplay = (gameboard, player1, player2) => {
     */
   }
 
-  const setPiece = () => {
-    console.log('Hello world!');
+  const setPiece = (piece, index) => {
+    gameboard.addPiece(piece, index)
+    console.log(gameboard.getGameboard());
   }
 
   return {
+    getGamemode,
     setPiece,
-    checkWinner
+    checkWinner,
+    startGame,
+    endGame
   }
 }
 
@@ -72,12 +113,24 @@ function Game(player1, player2) {
 const player1 = new Player();
 const player2 = new Player('Player 2', 'O');
 const gameboard = new Gameboard();
+const game = new Gameplay(gameboard, player1, player2);
 
-const game = new Game(player1, player2);
 console.log(player1.hello());
-console.log(game.game());
 console.log(gameboard.getGameboard());
 console.log(gameboard.addPiece('X', 2));
 console.log(gameboard.addPiece('O', 0));
 console.log(gameboard.addPiece('X', 6));
 console.log(gameboard.addPiece('O', 2));
+
+BUTTON.addEventListener('click', function(event) {
+  console.log(game.getGamemode());
+  game.startGame();
+  console.log(game.getGamemode());
+});
+for (let i = 0; i < GAMESPACES.length; i++) {
+  GAMESPACES[i].addEventListener('click', function(event) {
+    if (game.getGamemode()) {
+      game.setPiece(player1.piece, GAMESPACES[i].dataset.position);
+    }
+  });
+}
